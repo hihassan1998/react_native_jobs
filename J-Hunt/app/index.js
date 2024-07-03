e57d4b1d15f34d30e90b46e933f53ea04911a342
hihassan1998 <hassanihussain1998@gmail.com>
2024-07-03 02:51:43 +0200

    folder structure according to clean coding practice created

---
+++ J-Hunt/app/index.js
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>
+        HOME
+        
+        Edit app/index.tsx to edit this screen.
+        </Text>
+    </View>
+  );
+}
