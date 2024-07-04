f5dd7becd5b3c02c68859732ed4d3c0e66ff2996
hihassan1998 <hassanihussain1998@gmail.com>
2024-07-04 02:52:35 +0200

    Updated the directory structure and upadeted the home page by aadding buttons

---
+++ app/index.js
@@ -0,0 +1,43 @@
+import { SafeAreaView, Text, View, ScrollView} from "react-native";
+import { useState } from 'react';
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+const Home = () => {
+  const router = useRouter();
+
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}
+    >
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn  iconUrl={icons.menu} dimentions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn  iconUrl={icons.profile} dimentions="100%" />
+          ),
+          headerTitle:""
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+        style={{
+          flex:1,
+          padding: SIZES.medium
+        }}
+        >
+
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default Home;
