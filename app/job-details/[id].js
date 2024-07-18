503d9a71c8938d4bb450ed532ac654337616fbd3
hihassan1998 <hassanihussain1998@gmail.com>
2024-07-18 10:56:15 +0200

    creating dynamic routes to the nearbyjobcarddetails

---
+++ app/job-details/[id].js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl
+} from 'react-native';
+import { Stack, useRouter, useLocalSearchParams, useGlobalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import { useFetch } from '../../hook/useFetch';
+
+const JobDetails = () => {
+    const params = useLocalSearchParams();
+    const router = useRouter();
+    const { data, isLoading, error, refresh } = useFetch / ('job-details',
+        { job_id: params.id })
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = () => { }
+
+    return (
+        <SafeAreaView
+            style={{ flex: 1, backgroundColor: COLORS.lightWhite }}
+        >
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back}
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.share}
+                            dimension="60%"
+                        />
+                    ),
+                    headerTitle: ''
+                }}
+            />
+
+            <>
+                <ScrollView showsVerticalScrollIndicator={false}
+                    refreshControl={< RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+                >
+                    {isLoading ? (
+                        <ActivityIndicator size='large' color={COLORS.primary} />
+                    ) : error ? (
+                        <Text>Something went wrong</Text>
+                    ) : data.length === 0 ? (
+                        <Text>No data available</Text>
+                    ) : (
+                        <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                            <Company
+                                companyLogo={data[0].employer_logo}
+                                jobTitle={data[0].jobTitle}
+                                companyName={data[0].employer_name}
+                                Location={data[0].job_contry}
+
+                            />
+
+                            <JobTabs
+
+                            />
+                        </View>
+                    )}
+
+                </ScrollView>
+            </>
+        </SafeAreaView>
+    )
+}
+
+export default JobDetails
